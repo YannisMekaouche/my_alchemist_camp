7e1265b806b83591e78b173c2a926eb04aa6b25b
YannisMekaouche <yaanis213@gmail.com>
2019-03-08 12:28:59 +0100

    fix challenge3.ex

--- challenge3.ex
+++ challenge3.ex
@@ -3,12 +3,13 @@ defmodule Challenge3 do
   def get_files() do
     {:ok, files} = File.ls
     Enum.each files, fn file ->
-      [ file_extension] = tl String.split(file, ".")
-      file_extension = List.to_string(extension)
-      if Regex.match?(~r/[jpg]|[png]|[bmp]|[gif]/, file_extension) do
-        file_extension
-        #move_file(file)
+      extension = tl String.split(file, ".")
+      string_extension = List.to_string(extension)
+
+      if Regex.match?(~r/[jpg]|[png]|[bmp]|[gif]/, string_extension) do
+        move_file(file)
       end
+
     end
   end
 
